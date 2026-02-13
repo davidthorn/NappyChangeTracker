fdb30207419f094e1cd5b4d41ec75737b5255fc9
David Thorn <david.thorn221278@googlemail.com>
2026-02-13 13:47:56 +0100

    Initial Commit

---
+++ NappyChangeTracker/NappyChangeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NappyChangeTrackerApp.swift
+//  NappyChangeTracker
+//
+//  Created by David Thorn on 13.02.26.
+//
+
+import SwiftUI
+
+@main
+struct NappyChangeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
